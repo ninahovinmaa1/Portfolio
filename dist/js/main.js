b3fb6953eb0d6fb90c18d4b2c18dde87e1d9e2e6
Nina <ninahovinmaa@gmail.com>
2020-11-13 12:09:22 +0100

    added stuff to education page, other minor accessibility changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,39 @@
-const menuBtn = document.querySelector(".menu-btn"); //wrapper
-const hamburger = document.querySelector(".menu-btn__burger") //span
+//Element selectors, menu & hamburger
+const menuBtn = document.querySelector(".menu-btn");
+const hamburger = document.querySelector(".menu-btn__burger") 
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
 const navItems = document.querySelectorAll(".menu-nav__item");
 
+
+/*adds functionality to hamburger navigation when clicked. Open, close, add/remove classes.*/
+//default value, showMenu is false
 let showMenu = false;
 
+//when menuBtn is clicked, invoke function toggleMenu
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     //if showMenu is false
-    if(!showMenu) {         
+    if(!showMenu) {     
+        //add classes to selected elements    
         hamburger.classList.add("open");
         nav.classList.add("open");
         menuNav.classList.add("open");
         navItems.forEach(item => item.classList.add("open"));
 
+        //change showMenu to true
         showMenu = true;
+
+    //if showMenu is true
     } else {
+        //remove classes from selected elements
         hamburger.classList.remove("open");
         nav.classList.remove("open");
         menuNav.classList.remove("open");
         navItems.forEach(item => item.classList.remove("open"));
 
+        //change showMenu to false
         showMenu = false;
     }
 }
